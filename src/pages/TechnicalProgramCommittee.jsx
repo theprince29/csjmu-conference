eb3e94037b43ec36c6d790dee6adcfbd4502a0da
theprince29 <princepalvns007@gmail.com>
2024-10-29 11:01:53 +0530

    add technical Program committee

---
+++ src/pages/TechnicalProgramCommittee.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TechnicalProgramCommittee = () => {
+  return (
+    <div className="bg-gray-100 text-left p-6 md:p-12 lg:p-16">
+      <h1 className="text-4xl font-bold text-center text-third mb-8">Technical Program Committee</h1>
+      <ul className="space-y-2 text-gray-700">
+        <li>Prof Anita Yadav, HBTU, Kanpur</li>
+        <li>Prof Vandana Dixit Kaushik, HBTU, Kanpur</li>
+        <li>Prof A K Shankhwar, HBTU, Kanpur</li>
+        <li>Prof Ashutosh Singh, HBTU, Kanpur</li>
+        <li>Dr. Y N Singh, Director, REC, Pratapgarh</li>
+        <li>Dr Rashi Agarwal, HBTU, Kanpur</li>
+        <li>Dr. R C S Chauhan, IET, Lucknow</li>
+        <li>Dr. Vibhash Yadav, REC, Banda</li>
+        <li>Dr. Keshav Kaushik, Amity Uni., Punjab</li>
+        <li>Dr Pronaya Bhattacharya, Amity Uni., Kolkata</li>
+        <li>Dr Deepak Kumar, Chitkara Uni., Punjab</li>
+        <li>Prof Saurabh Singh, Galgotias University</li>
+        <li>Dr Prabhsimran Singh, G N D Uni., Amritsar</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TechnicalProgramCommittee;
