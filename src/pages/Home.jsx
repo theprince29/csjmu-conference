2f16147a03e072a4805f3ed8488854d7bcaa1129
theprince29 <princepalvns007@gmail.com>
2024-11-16 15:02:10 +0530

    added new description and keywords

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Home - ICRAECCT'2025 Conference</title>
+        <title>ICRAECCT'2025 Conference</title>
         <meta
           name="description"
-          content="Join us at the ICRAECCT 2025 Conference hosted by Chhatrapati Shahu Ji Maharaj University, Kanpur. Discover pioneering advancements in computing and communication technologies."
+          content="Join the 1st International Conference on Recent Advances in Emerging Computing and Communication Technologies (IC-RAECCT 2025) at Chhatrapati Shahu Ji Maharaj University, Kanpur. Held on April 11-12, 2025, this hybrid event is organized by the Departments of Computer Applications, Computer Science & Engineering, and Electronics & Communication Engineering. Explore advancements in AI, IoT, cybersecurity, 5G, data science, and more. Engage with experts, attend workshops, and network with industry leaders at CSJMU's esteemed campus."
         />
         <meta
           name="keywords"
-          content="ICRAECCT 2025, computing conference, communication technologies,csjmu conference, CSJMU, Kanpur, technology advancements, academic conference, research, computing, communication"
+          content="CSJMU conference 2025, CSJM university upcoming conferences,  ICRAECCT 2025, ICRAECCT, UIET conference,CSE conference,CSE Department UIET CSJM University kanpur ,ICRAECCT 2025, computing conference, communication technologies,csjmu conference, CSJMU, Kanpur, technology advancements, academic conference, research, computing, communication"
         />
       </Helmet>
       <Carousel />
