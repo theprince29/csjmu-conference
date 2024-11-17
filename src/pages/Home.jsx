a07d157f423097bde7db0a34625961c13dfbdb93
theprince29 <princepalvns007@gmail.com>
2024-11-17 11:19:51 +0530

    added image link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <title>ICRAECCT'2025 Conference</title>
         <meta
           name="description"
-          content="Join the 1st International Conference on Recent Advances in Emerging Computing and Communication Technologies (IC-RAECCT 2025) at Chhatrapati Shahu Ji Maharaj University, Kanpur. Held on April 11-12, 2025, this hybrid event is organized by the Departments of Computer Applications, Computer Science & Engineering, and Electronics & Communication Engineering. Explore advancements in AI, IoT, cybersecurity, 5G, data science, and more. Engage with experts, attend workshops, and network with industry leaders at CSJMU's esteemed campus."
+          content="Welcome to International Conference on Recent Advances in Emerging Computing and Communication Technologies (IC-RAECCT 2025) at Chhatrapati Shahu Ji Maharaj University, Kanpur. Held on April 11-12, 2025, this hybrid event is organized by the Departments of Computer Applications, Computer Science & Engineering, and Electronics & Communication Engineering. Explore advancements in AI, IoT, cybersecurity, 5G, data science, and more. Engage with experts, attend workshops, and network with industry leaders at CSJMU's esteemed campus."
         />
         <meta
           name="keywords"
