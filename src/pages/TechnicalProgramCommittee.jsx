8305c72d6ed699a9c2c621cfcc8991825bc61910
theprince29 <princepalvns007@gmail.com>
2024-11-23 00:40:29 +0530

    updated new list 23/11

--- src/pages/TechnicalProgramCommittee.jsx
+++ src/pages/TechnicalProgramCommittee.jsx
@@ -22,7 +22,7 @@ const TechnicalProgramCommittee = () => {
         </li>
         <li>Prof. Simanta Mitra, Iowa State University, USA</li>
         <li>
-          Prof. Syed Akhter Hossain, Faculty of Science & Information Technology
+          Prof. Syed Akhter Hossain, Faculty of Science &Information Technology
           (FSIT) Daffodil International University (DIU), Bangladesh
         </li>
         <li>
@@ -70,7 +70,7 @@ const TechnicalProgramCommittee = () => {
           AI), Bournemouth University, UK
         </li>
         <li>
-          Dr. Muhammad Anshari bin Ali, Universiti Brunei Darussalam, Brunei
+          Dr Muhammad Anshari bin Ali, Universiti Brunei Darussalam, Brunei
         </li>
         <li>
           Dr. Nahed Tawfik, Computers and Systems Department, Electronics
@@ -99,19 +99,19 @@ const TechnicalProgramCommittee = () => {
           (Main Campus), Kufa, Iraq
         </li>
         <li>
-          Yasmin Makki Mohialden, Computer Science Department, College of
+          Yasmin Makki Mohialden Computer Science Department, Collage of
           Science, Mustansiriyah University, Baghdad-Iraq
         </li>
         <li>
           Dr. Haider M. Al-Juboori, Dept. of Electronics Engineering and
           Communications, Faculty of Engineering & Built Environment, South East
-          Technological University, Ireland
+          Technological University, Ireland,
         </li>
         <li>Dr. Nuno Mateus-Coelho, Lusófona University, Porto, Portugal</li>
         <li>Dr. Parvez Ali, Qassim University, Unaizah, Saudi Arabia</li>
         <li>
           Dr. Hari Mohan Gaur, Frankfurt Institute for Advanced Studies,
-          Goethe-Uni Frankfurt, Germany
+          Goethe-Uni Frankfurt , Germany.
         </li>
         <li>
           Dr. Rajeev Kumar Shakya, Department of Electronics and Communication
@@ -127,7 +127,7 @@ const TechnicalProgramCommittee = () => {
           Nishchai J M - Amazon Web Services (AWS), Redmond, Washington, United
           States
         </li>
-        <li>Dr. Hameed Mutlag Farhan, Altinbas University, Turkey</li>
+        <li>Dr. Hameed Mutlag Farhan Altinbas University, Turkey</li>
         <li>Er. Himani, Wipro Limited, Project Manager, Chicago, IL, USA</li>
         <li>
           Er. Nirav Narendra Kumar Modh, Infosys Limited, Chicago, IL, USA
@@ -157,6 +157,282 @@ const TechnicalProgramCommittee = () => {
           Prof. (Dr.) Deepa Raj, School of Information Science and Technology,
           Baba Saheb Bhimrao Ambedkar University, Lucknow (A Central University)
         </li>
+        <li>Prof Raghuraj Singh, HBTU Kanpur</li>
+        <li>Prof N K Kohli, HBTU Kanpur</li>
+        <li>
+          Prof. Rajeev Kumar Upadhyay, Director, KNIT, Sultanpur (UP), India
+        </li>
+        <li>
+          Prof. Neelendra Badal, Director, Rajkiya Engineering College, Bijnor
+          (UP), India
+        </li>
+        <li>
+          Prof. Vikram Bali, Director, Galgotias College of Engineering &
+          Technology (GCET), Gr. Noida (UP), India
+        </li>
+        <li>
+          Prof. Shivani Bali, Professor-Business Analytics, Jaipuria Institute
+          of Management, Noida (UP), India
+        </li>
+        <li>
+          Prof. Sumit Kumar, Dept. of CSE, ASET, Amity University, Noida (UP),
+          India
+        </li>
+        <li>
+          Prof. Laxmi Ahuja, Dept. of IT, AIIT, Amity University, Noida (UP),
+          India
+        </li>
+        <li>Prof. Praveen Gupta, Director, LBSIM, New Delhi, India</li>
+        <li>Prof. Rekha Gupta, Professor-IT, LBSIM, New Delhi, India</li>
+        <li>
+          Prof. Sachin Ahuja, Executive Director - Engineering, Chandigarh
+          University, Chandigarh, India
+        </li>
+        <li>Prof. K. K. Goyal, Dean-MCA, RBSMTC, Khandari, Agra (UP), India</li>
+        <li>
+          Prof. Pankaj Saxena, Professor-MCA, RBSMTC, Khandari, Agra (UP), India
+        </li>
+        <li>
+          Prof. Adesh Kr. Pandey, Dean & Professor-IT, KIET, Ghaziabad (UP),
+          India
+        </li>
+        <li>
+          {" "}
+          Prof. Vineet Sharma, Dean & Professor-CSE, KIET, Ghaziabad (UP), India
+        </li>
+        <li>
+          Prof. Raghav Mehra, Dept. of CSE, Chandigarh University, Chandigarh,
+          India
+        </li>
+        <li>
+          Prof. Charu Pathak, Dept. of Electronics & Communication, Manav Rachna
+          University, Faridabad, Haryana, India
+        </li>
+        <li>
+          Prof. Suneet Gupta, SCSE, Galgotias University, Gr. Noida (UP), India
+        </li>
+        <li>
+          Prof. Anu Chaudhary, Head-Dept. of IT, AKGEC, Ghaziabad (UP), India
+        </li>
+        <li>Prof. M. N. Hoda, Director, BVICAM, New Delhi, India</li>
+        <li>
+          Prof. Gur Saran, Professor, Dayalbagh Educational Institute (DEI),
+          Dayalbagh, Agra (UP), India
+        </li>
+        <li>Prof. Arun Sharma, Dean-Academics, IGDTUW, New Delhi, India</li>
+        <li>
+          Prof. R. K. Singh, Professor, Dept. of IT & Dean (Students' Welfare),
+          IGDTUW, New Delhi, India
+        </li>
+        <li>
+          Prof. Amar K. Mohapatra, Head-Department of IT, IGDTUW, New Delhi,
+          India
+        </li>
+        <li>Prof. M. U. Bokhari, Dept. of CS, AMU, Aligarh (UP), India</li>
+        <li>
+          Prof. Vinay Rishiwal, Professor, Dept. of CSIT, MJP Rohilkhand
+          University, Bareilly (UP), India
+        </li>
+        <li>
+          Prof. Vrijendra Singh, Professor, Dept. of IT, IIIT-Allahabad (UP),
+          India
+        </li>
+        <li>
+          Prof. D. P. Mohapatra, Professor, Dept. of CSE, NIT Rourkela, Odisha,
+          India
+        </li>
+        <li>
+          Prof. Abhay Bansal, Dean, Engineering (SCSET & SEAS) & Dean (IRCO),
+          Bennett University, Greater Noida (UP), India
+        </li>
+        <li>Prof. Sanjeev Kumar, Professor, SCS, UPES, Dehradun</li>
+        <li>Prof. (Dr.) Anita Yadav, HBTU, Kanpur</li>
+        <li>Prof. (Dr.) Vandana Dixit Kaushik, HBTU, Kanpur</li>
+        <li>Prof. (Dr.) A K Shankhwar, HBTU, Kanpur</li>
+        <li>Prof. (Dr.) Rachna Asthana, HBTU, Kanpur</li>
+        <li>Prof. (Dr.) Y N Singh, Director, REC, Pratapgarh</li>
+        <li>Prof. (Dr.) Ashutosh Singh, HBTU, Kanpur</li>
+        <li>Prof. (Dr.) Manoj Kumar Shukla, HBTU, Kanpur</li>
+        <li>
+          Prof. (Dr.) Ajai Jain, Institute of Engineering and Technology, JK
+          Lakshmipat University, Jaipur.
+        </li>
+        <li>
+          Prof. (Dr.) Renu Jain, Institute of Engineering and Technology, JK
+          Lakshmipat University, Jaipur.
+        </li>
+        <li>
+          Prof. (Dr.) R. K. Dwivedi, Director, Teerthanker Mahaveer University,
+          Moradabad
+        </li>
+        <li>
+          Prof. (Dr.) Brijesh Kumar, Professor & Director (Planning), Indira
+          Gandhi Delhi Technical University For Women, Delhi
+        </li>
+        <li>
+          Prof. R. P. Mahapatra, SRMIST, Delhi-NCR Campus, Ghaziabad (UP), India
+        </li>
+        <li>
+          Prof. Jagadeesh G., Professor, Software & Systems Engineering,
+          SCSE&IS, VIT, Vellore, Tamil Nadu, India
+        </li>
+        <li>
+          Dr. Ravindra Nath Katiyar, Baba Saheb Bhimrao Ambedkar University,
+          Lucknow (A Central University)
+        </li>
+        <li>
+          Prof. A. Senthil, K L University, Vaddeswaram, Andhra Pradesh, India
+        </li>
+        <li>Prof. Shamik Tiwari, IILM University, Gurugram, Haryana, India</li>
+        <li>
+          Prof. Munish Khanna, Galgotias University, Greater Noida (UP), India
+        </li>
+        <li>Prof. Vishal Jain, Sharda University, Greater Noida (UP), India</li>
+        <li>Prof. Sachin Gupta, MAIT, GGSIPU, New Delhi, India</li>
+        <li>Prof. Abhishek Swaroop, BPIT, GGSIPU, New Delhi, India</li>
+        <li>
+          Prof. Karmeshu, Distinguished Professor, SCS, UPES, Dehradun and
+          Ex-Professor, SCSS, JNU, New Delhi, India
+        </li>
+        <li>
+          Prof. Adesh Kumar, University of Petroleum and Energy Studies,
+          Dehradun
+        </li>
+        <li>Prof. Anurag Tripathi, IET Lucknow</li>
+        <li>
+          Prof. B. K. Sharma, Professor, Dept. of MCA, AKGEC, Ghaziabad (UP),
+          India
+        </li>
+        <li>
+          Prof. Pradeep Kumar Gupta, Mohan Babu University, Tirupati, Andhra
+          Pradesh, India
+        </li>
+        <li>
+          Prof. Sunil Kr. Pandey, Director (IT & UG), ITS, Ghaziabad, India
+        </li>
+        <li>
+          Prof. (Dr.) Prashant Kumar Mishra, Pranveer Singh Institute of
+          Technology, Kanpur
+        </li>
+        <li>
+          Prof. (Dr.) Manoj Diwakar, Graphic Era Deemed to be University, India
+        </li>
+        <li>
+          Prof. (Dr.) Ganga Ram Mishra, Dr. Rammanohar Lohia Avadh University,
+          Ayodhya
+        </li>
+        <li>Prof. (Dr.) Manish Kumar, BBD University, Lucknow</li>
+        <li>Prof. (Dr.) Saurabh Singh, Galgotias University</li>
+        <li>
+          Prof. V M Mishra, G.B. Pant Engineering College, Garhwal, Uttarakhand
+        </li>
+        <li>Dr Vibhav Prakash Singh, MNNIT Allahabad</li>
+        <li>Dr. Tarun Kumar Gupta, University of Allahabad</li>
+        <li>Dr. Ram Chandra Singh Chauhan, IET Lucknow</li>
+        <li>Dr. Rashi Agarwal, HBTU, Kanpur</li>
+        <li>Dr. Pronaya Bhattacharya, Amity Uni., Kolkata</li>
+        <li>Dr. Rajender Kumar, NIT Kurukshetra, Haryana</li>
+        <li>Dr. Vibhash Yadav, REC, Banda</li>
+        <li>Dr. Keshav Kaushik, Amity Uni., Punjab</li>
+        <li>Dr Prabhsimran Singh, G N D Uni., Amritsar</li>
+        <li>Dr Imran Khan HBTU Kanpur</li>
+        <li>Dr. Sudhanshu Verma, MMMUT Gorakhpur</li>
+        <li>
+          Dr. Pramod Kumar Yadav, National Institute of Technology Srinagar
+        </li>
+        <li>
+          Dr. Raghvendra Kumar Chaudhary, Indian Institute of Technology (IIT)
+          Kanpur, India
+        </li>
+        <li>Dr. Deepak Gupta, MNNIT Allahabad, India</li>
+        <li>
+          Mr. Kural, Chief scientist, Central Drug Research Institute, Lucknow,
+          India
+        </li>
+        <li>
+          Dr. Shrawan Kumar, Indira Gandhi National Tribal University Regional
+          Campus Manipur India
+        </li>
+        <li>
+          Dr. Manoj Kumar Misra, Director, Allenhouse Institute of Technology,
+          Kanpur
+        </li>
+        <li>
+          Dr. Shrawan Trivedi, Rajiv Gandhi Institute of Petroleum Technology,
+          Amethi, India
+        </li>
+        <li>Dr. Ajay Narayan Shukla, Graphic Era Hill University, Dehradun</li>
+        <li>Dr Binu Dennis, Resbee Info Technologies, India</li>
+        <li>Dr B R Rajakumar, Resbee Info Technologies, India</li>
+        <li>Dr Deepak Kumar, Chitkara Uni., Punjab</li>
+        <li>Dr. Kapil Joshi, Uttaranchal University, India</li>
+        <li>Dr. Vivek Srivastava, REC Kannauj, India</li>
+        <li>Dr Shailendra Kumar, Integral University, Lucknow</li>
+        <li>Dr. Siddharth Singh, University of Lucknow, Lucknow</li>
+        <li>Dr. Zeeshan Ali Siddiqui, University of Lucknow, Lucknow</li>
+        <li>Dr Saurabh Pandey, Vardhman Mahaveer Open University, Kota</li>
+        <li>Dr. Vinolin V, Resbee Info Technologies, India</li>
+        <li>Dr. Abhishek Prabhakar, AITH Kanpur</li>
+        <li>Dr. K K Singh, Amity University, Lucknow</li>
+        <li>Dr. Deependra Pandey, Amity University, Lucknow</li>
+        <li>Dr. Himanshu Katiyar, Rajkiya Engineering College Sonbhadra</li>
+        <li>Dr. Monica Mehrotra, Director, B.B.D. ESGI Lucknow</li>
+        <li>
+          Dr. Bhupendra Singh, Scientist, Secure Syatems Division, CAIR, DRDO,
+          Bengaluru, India
+        </li>
+        <li>
+          Dr. Ashish Dubey, Sr. Scientist - DMSRDE, DRDO, Kanpur (UP), India
+        </li>
+        <li>
+          Dr. Biswajit Saha, Director (Trainings & Skill Education), CBSE, New
+          Delhi, India
+        </li>
+        <li>
+          Dr. Atul Chaturvedi, Dy. Director, Allenhouse Institute of Technology
+          (AIT), Rooma, India
+        </li>
+        <li>S.H. Abbas Mehdi, Dy. Director, STPI, Indore (MP), India</li>
+        <li>Dr. Himanshu Mittal, IGDTUW, New Delhi, India</li>
+        <li>Dr. Shekhar Shukla, IIM Indore, India</li>
+        <li>Dr. Praveen Ranjan Srivastava, IIM Rohtak, India</li>
+        <li>
+          Dr. Rupali Bhardwaj, Thapar Institute of Engineering & Technology,
+          Patiala, Punjab, India
+        </li>
+        <li>Dr. Karan Singh, SCSS, JNU, New Delhi, India</li>
+        <li>Dr. Syed Imtiyaz Hassan, SCSIT, MANUU, Hyderabad, India</li>
+        <li>Dr. Abhay Kumar Singh, IIT (ISM), Dhanbad, Jharkhand, India</li>
+        <li>Dr. Uday Pratap Rao, NIT, Patna, Bihar, India</li>
+        <li>Dr. Vivek Singh Verma, HBTU, Kanpur (UP), India</li>
+        <li>Dr. B. B. Sagar, HBTU, Kanpur (UP), India</li>
+        <li>
+          Dr. Pradeep Kumar Singh, Central University of Jammu, J&K, India
+        </li>
+        <li>Dr. Nitish Pathak, BPIT, GGSIPU, New Delhi, India</li>
+        <li>Dr. Ashish Khanna, MAIT, GGSIPU, New Delhi, India</li>
+        <li>Dr. Chandramani Sharma, SCS, UPES, Dehradun (UK), India</li>
+        <li>
+          Dr. Arun Solanki, USICT, Gautam Buddha University (GBU), Gr. Noida
+          (UP), India
+        </li>
+        <li>
+          Dr. Pradeep Tomar, USICT, Gautam Buddha University (GBU), Gr. Noida
+          (UP), India
+        </li>
+        <li>Dr. Alka Agrawal, BBAU, Lucknow (UP), India</li>
+        <li>Dr. Neetu Gupta, AIIT, Amity University, Noida (UP), India</li>
+        <li>
+          Dr. Arnika Jain, Manav Rachna University, Faridabad, Haryana, India
+        </li>
+        <li>
+          Dr. Avneesh Vashistha, SRMIST, Delhi-NCR Campus, Ghaziabad (UP), India
+        </li>
+        <li>
+          Dr. Jitendra Singh, SRMIST, Delhi-NCR Campus, Ghaziabad (UP), India
+        </li>
+        <li>Dr. Prashant Giridhar Shambharkar, DTU, New Delhi, India</li>
+        <li>Mr. Ibrahim Hayatu Hassan, Ahmadu Bello University, Nigeria</li>
       </ul>
     </div>
   );
