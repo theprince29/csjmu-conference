5fab4983b3695268aed55d71d4d200295a613ac6
theprince29 <princepalvns007@gmail.com>
2024-11-23 01:23:26 +0530

    keynote speakers 2 persons in a row (total 8 speakers) dont mix text...just use row wise..as given in word file

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -5,6 +5,15 @@ import * as Images from "../assets";
 
 
 const SpeakerCard = ({ name, title, description, image }) => {
+
+  const renderDescription = (description) => {
+    return description.split("\n").map((line, index) => (
+      <p key={index} className="text-gray-500 text-xs mt-2">
+        {line}
+      </p>
+    ));
+  };
+
     return (
       <div className="bg-white shadow-lg rounded-lg p-4 text-center">
         <img
@@ -14,7 +23,7 @@ const SpeakerCard = ({ name, title, description, image }) => {
         />
         <h3 className="text-lg font-bold text-primary mt-4">{name}</h3>
         <p className="text-gray-600 text-sm mt-1">{title}</p>
-        <p className="text-gray-500 text-xs mt-2">{description}</p>
+        <p className="text-gray-500  text-xs mt-2">{renderDescription(description)}</p>
       </div>
     );
   };
@@ -23,7 +32,7 @@ const SpeakerCard = ({ name, title, description, image }) => {
     {
       name: "Prof. Alvaro Rocha",
       title:  "Professor of Information Systems, ISEG, University of Lisbon, Portugal",
-      description:"World's Top 1% Scientist, according to Stanford University and Elsevier" ,
+      description:"World's Top 1% Scientist, according to Stanford University and Elsevier \n ISEM's Book Series Scientific Manager at Springer Nature \n Chair of ITMA - Information and Technology Management Association \n Founder and Vice-Chair of IEEE SMC Portugal Chapter \n Invited Professor at University of Calabria, Italy \n CEO at AMARITS Consulting, Dubai",
       image: Images.ProfAlvaroRocha,
     },
     {
@@ -65,7 +74,7 @@ const SpeakerCard = ({ name, title, description, image }) => {
     {
       name: "Dr. Govind P Gupta",
       title: "NIT Raipur",
-      description: "Enlisted in Global AD (Alper-Doger) Scientific Index 2024 list. Ranked within the 'Top 2% Global Scientists/Researchers in the World' (Published by Elsevier, on October 4, 2023)",
+      description: "Enlisted in Global AD (Alper-Doger) Scientific Index 2024 list.\nRanked within the 'Top 2% Global Scientists/Researchers in the World' (Published by Elsevier, on October 4, 2023)",
       image: Images.DrGovindPGupta,
     },
     
@@ -88,20 +97,20 @@ const SpeakerCard = ({ name, title, description, image }) => {
 
 const Speakers = () => {
   return (
-    <div className="bg-gray-100 p-8 text-left mt-4 rounded-lg shadow-md  list-inside">
-      <h1 className="text-4xl font-bold text-center text-third mb-8 uppercase">Keynote Speakers</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-        {speakers.map((speaker, index) => (
-          <SpeakerCard
-            key={index}
-            name={speaker.name}
-            title={speaker.title}
-            description={speaker.description}
-            image={speaker.image}
-          />
-        ))}
-      </div>
+    <div className="bg-gray-100 p-8 text-left mt-4 rounded-lg shadow-md list-inside">
+    <h1 className="text-4xl font-bold text-center text-third mb-8 uppercase">Keynote Speakers</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 max-w-5xl mx-auto">
+      {speakers.map((speaker, index) => (
+        <SpeakerCard
+          key={index}
+          name={speaker.name}
+          title={speaker.title}
+          description={speaker.description}
+          image={speaker.image}
+        />
+      ))}
     </div>
+  </div>
   )
 }
 
