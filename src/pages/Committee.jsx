6dd71d6d7989ca4e4ffe3abf444a8d2aeea0160c
theprince29 <princepalvns007@gmail.com>
2024-11-23 00:57:44 +0530

    updated steering list 23/11

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -3,12 +3,15 @@ import { Helmet } from "react-helmet-async";
 const Committee = () => {
   return (
     <div className="bg-gray-100 text-left p-6 md:p-12 lg:p-16">
-       <Helmet>
+      <Helmet>
         <title>Steering Committee - ICRAECCT'2025 Conference</title>
-        <meta name="description" content="Meet the steering committee members of ICRAECCT'2025 Conference" />
+        <meta
+          name="description"
+          content="Meet the steering committee members of ICRAECCT'2025 Conference"
+        />
       </Helmet>
       <h1 className="text-4xl font-bold text-center text-third mb-8">
-        STEERING COMMITTEE 
+        STEERING COMMITTEE
       </h1>
 
       <div className="space-y-8 space-x-8">
@@ -37,7 +40,7 @@ const Committee = () => {
 
         <section>
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Executive Chair
+            Co-Patron
           </h2>
           <ul className="list-disc list-inside text-gray-700">
             <li>
@@ -51,16 +54,91 @@ const Committee = () => {
             General Chair
           </h2>
           <ul className="list-disc list-inside text-gray-700">
-            <li>Dr. Deepak Kumar Verma</li>
-            <li>Dr. Vishal Awasthi</li>
-            <li>Dr. Sandesh Gupta</li>
-            <li>Dr. Neeraj Kumar</li>
+            <li>
+              Prof. (Dr.) Valentina Emilia Balas, Aurel Vlaicu University of
+              Arad / Academy of Romanian Scientists, ROMANIA
+            </li>
+            <li>
+              Prof. Sergei Levashkin, Artificial Intelligence Research Lab
+              (AIRL), Povolzhskiy State University of Telecommunications and
+              Informatics (PSUTI), Samara, Russia
+            </li>
+            <li>Prof. Rajeev Srivastava, IIT BHU</li>
+            <li>Prof. Prabodh Bajpai, IIT Kanpur</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Program Chair
+          </h2>
+          <ul className="list-disc list-inside text-gray-700">
+            <li>
+              Prof. (Dr.) Valentina Emilia Balas, Aurel Vlaicu University of
+              Arad / Academy of Romanian Scientists, ROMANIA
+            </li>
+            <li>Prof. (Dr.) Pabitra Mitra, IIT BHU</li>
+            <li>Dr. Hari Mohan Pandey, Bournemouth University UK</li>
+            <li>Dr. Mohd Anshari Bin Ali, Universiti Brunei Darussalam</li>
           </ul>
         </section>
 
         <section>
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">
             Organizing Chair
+          </h2>
+          <ul className="list-disc list-inside text-gray-700">
+            <li>Dr. Raghvendra Kumar Chaudhary, IIT Kanpur</li>
+            <li>Dr. Vipul Arora, IIT Kanpur</li>
+            <li>Dr. Ruchir Gupta, IIT BHU</li>
+            <li>Dr. Praveen Tripathi, IIT Guwahati</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Editorial Chair
+          </h2>
+          <ul className="list-disc list-inside text-gray-700">
+            <li>Prof. Rajeev Srivastava, IIT BHU</li>
+            <li>Dr. Mohd Anshari Bin Ali, Universiti Brunei Darussalam</li>
+            <li>Dr. Kapil Joshi, Uttaranchal University Dehradun</li>
+            <li>Prof. Rabins Porwal, CSJM University Kanpur</li>
+            <li>Dr. Vineeta Singh, CSJM University Kanpur</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Publication Chair
+          </h2>
+          <ul className="list-disc list-inside text-gray-700">
+            <li>
+              Prof. (Dr.) Valentina Emilia Balas, Aurel Vlaicu University of
+              Arad / Academy of Romanian Scientists, ROMANIA
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Publicity Chair
+          </h2>
+          <ul className="list-disc list-inside text-gray-700">
+            <li>
+              Dr. Nahed Tawfik Electronics Research Institute, Cairo Egypt
+            </li>
+            <li>Dr. Nuno Mateus Coelho, Lusofona University, Portugal</li>
+            <li>Dr. Ankush Sharma, IIT Kanpur</li>
+            <li>Dr. Deepak Gupta, MAIT Delhi</li>
+            <li>Dr. Keshav Kaushik, Amity University Punjab</li>
+            <li>Dr. Kapil Joshi Uttaranchal, University Dehradun</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Convenor(s)
           </h2>
           <ul className="list-disc list-inside text-gray-700">
             <li>Prof. Rabins Porwal</li>
@@ -71,131 +149,63 @@ const Committee = () => {
 
         <section>
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Co-Chair
+            Co-Convenor(s)
           </h2>
           <ul className="list-disc list-inside text-gray-700">
+            <li>Dr. Pushpa Mamoriya</li>
             <li>Dr. Vineeta Singh</li>
-            <li>Dr. Pushpa Memoriya</li>
-            <li>Mrs. Parul Awasthi</li>
-            <li>Mr. Amit Virmani</li>
             <li>Dr. Ajay Tiwari</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Website</h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Mr. Vijay Agrawal</li>
-            <li> Mr. Shah Alam</li>
-            <li>Mr. Arpit Dubey</li>
-            <li>Mr. Prateek Srivastava</li>
-            <li>Ms. Suruchi Singh</li>
-            <li>Mr. Amit K Katiyar</li>
-            <li>Mr. Prince Pal</li>
-            <li>Mr. Sujit Kumar</li>
-            <li>Mr. Shashi Kant</li>
-            <li>Mr. Aditya Raj</li>
+            <li>Dr. Kapil Joshi Uttaranchal, University Dehradun</li>
           </ul>
         </section>
 
         <section>
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Event Management Committee
+          Organizing Committee
           </h2>
           <ul className="list-disc list-inside text-gray-700">
-            <li> Prof. Rabins Porwal</li>
-            <li>Dr. Ajay Tiwari</li>
-            <li>Dr Arpita Singh</li>
-            <li>Mr. Atul Agnihotri</li>
-            <li>Mr. Arpit Dubey</li>
-            <li>Mr. Shilpi Dubey</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Public Relation Committee
-          </h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Dr. Sandesh Gupta</li>
-            <li>Dr. Ajay Tiwari</li>
-            <li>Mr. Sanjeer Kumar</li>
-            <li>Mr. Shivneet Tripathi</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Publicity
-          </h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Dr. Sandesh Gupta</li>
-            <li>Mr. Amit Virmani</li>
-            <li>Mr. Omkar Agrahari</li>
-            <li>Mr. Shah Alam</li>
-            <li>Mr. Atul Kr. Agnihotri</li>
-            <li>Mr. Prashant Srivastava</li>
-            <li>Mrs. Arpita Singh</li>
-            <li>Dr. Mayur Rahul</li>
-            <li>Mr Ravi Kant Mishra</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Hospitality Committee
-          </h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Dr. Pushpa Memoriya</li>
-            <li>Dr. Mamta Tiwari</li>
-            <li>Mr. Amit Virmani</li>
-            <li>Mr. Sheshmani Tiwari</li>
-            <li>Mr. Shah Alam</li>
-            <li>Mr. Amit Kr. Katiyar</li>
-            <li>Mr. Shilpi Dubey</li>
-            <li>Mrs. Preeti Singh</li>
-            <li>Mr. Ramnayan Mishra</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Publication and Printing
-          </h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Prof. Rabins Porwal</li>
-            <li>Dr. Vineeta Singh</li>
-            <li>Dr. Ompal</li>
-            <li>Mr. Somesh K Malhotra</li>
-            <li>Mr. Hemant Kumar</li>
-            <li>Mr. Sunil Kumar</li>
-            <li>Mr. Hrishab K Prajapati</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Registration Committee
-          </h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Mr. Anand Kumar Gupta</li>
-            <li>Dr. Arpita Singh</li>
-            <li>Mr. Shubhendra Singh</li>
-            <li>Mrs. Preeti Singh</li>
-            <li>Mr. Sanjay Singh</li>
-            <li>Mr. Ritesh Agrawal</li>
-            <li>Mr. Ravi Kant</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Finance Committee
-          </h2>
-          <ul className="list-disc list-inside text-gray-700">
-            <li>Mr. Shekhar Varma</li>
-            <li>Mr. Kamal Kant</li>
-            <li>Mr. Somesh K. Malhotra</li>
+          <li>Dr. Deepak Kumar Verma</li>
+          <li>Dr. Vishal Awasthi</li>
+          <li>Dr. Sandesh Gupta</li>
+          <li>Dr. Niraj Kumar</li>
+          <li>Dr. Ompal</li>
+          <li>Mr. Anand Kumar Gupta</li>
+          <li>Mrs. Parul Awasthi</li>
+          <li>Mr. Amit Katiyar</li>
+          <li>Mr. Somesh Malhotra</li>
+          <li>Mr. Atul Kumar Agnihotri</li>
+          <li>Mrs. Preeti Singh</li>
+          <li>Mr. Amit Virmani</li>
+          <li>Mr. Vijay Agrawal</li>
+          <li>Mohd. Shah Alam</li>
+          <li>Mr. Arpit Dubey</li>
+          <li>Mr. Prateek Srivastava</li>
+          <li>Ms. Suruchi Singh</li>
+          <li>Dr. Arpita Singh</li>
+          <li>Mr. Arpit Dubey</li>
+          <li>Mr. Shilpi Dubey</li>
+          <li>Dr. Ajay Tiwari</li>
+          <li>Mr. Sanjeer Kumar</li>
+          <li>Mr. Shivneet Tripathi</li>
+          <li>Mr. Omkar Agrahari</li>
+          <li>Mr. Prashant Srivastava</li>
+          <li>Mrs. Arpita Singh</li>
+          <li>Dr. Mayur Rahul</li>
+          <li>Mr Ravi Kant Mishra</li>
+          <li>Dr. Pushpa Mamoriya</li>
+          <li>Dr. Mamta Tiwari</li>
+          <li>Mr. Sheshmani Tiwari</li>
+          <li>Mr. Ramnayan Mishra</li>
+          <li>Mr. Hemant Kumar</li>
+          <li>Mr. Sunil Kumar</li>
+          <li>Mr. Hrishab K Prajapati</li>
+          <li>Dr. Arpita Singh</li>
+          <li>Mr. Shubhendra Singh</li>
+          <li>Mr. Sanjay Singh</li>
+          <li>Mr. Ritesh Agrawal</li>
+          <li>Mr. Ravi Kant</li>
+          <li>Mr. Shekhar Varma</li>
+          <li>Mr. Kamal Kant</li>
           </ul>
         </section>
       </div>
